c99b09be992bc721f952e1d5a46e32155660676a
Anupam Anand <anandanupam@Anupams-MacBook-Air.local>
2026-04-11 13:11:50 +0530

    create models

---
+++ src/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        require: [true, "Please enter your username"]
+
+    },
+    email:{
+        type: String,
+        require: true,
+        unique: true,
+        match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please use valid email"]
+
+    },
+    password:{
+        type: String,
+    },
+    role:{
+        type: String,
+        enum:["user","artist"],
+        default: "user"
+    }
+})  
+
+const userModel = mongoose.model("user", userSchema);
+
+export default userModel
