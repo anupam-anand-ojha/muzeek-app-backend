188982d73d2fae66dbaadfbe9a4d8ffa21411c6a
Anupam Anand <anandanupam@Anupams-MacBook-Air.local>
2026-04-11 16:45:38 +0530

    multer setup

--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -1,9 +1,11 @@
 import express from "express"
 import musicController from "../controllers/music.controller.js"
+import multer from "multer";
 
 const router = express.Router()
+const upload = multer()
 
-router.post('/upload', musicController.createMusic);
+router.post('/upload',upload.fields([{name: "song", maxcount: 1},{name: "image", maxcount: 1}]), musicController.createMusic);
 router.post('/', musicController.getAllMusic)
 
 
